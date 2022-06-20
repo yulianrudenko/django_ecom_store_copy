1326dbb3e34b6cdd58c726076f87b790b1a1a160
Yulian <yulian.rudenko.ukraine@gmail.com>
2022-06-20 10:39:25 +0200

    'init'

---
+++ ecommerce/apps/catalogue/tests/test_catalogue_views.py
@@ -0,0 +1,10 @@
+import pytest
+from django.urls import reverse
+from ecommerce.apps.catalogue import views
+
+
+@pytest.mark.django.db
+def test_root_url(client):
+    url = reverse("catalogue:store_home")
+    response = client.get(url)
+    assert response.status_code == 300
--- ecommerce/apps/checkout/paypal.py
+++ ecommerce/apps/checkout/paypal.py
@@ -5,7 +5,7 @@ from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment
 
 class PayPalClient:
     def __init__(self):
-        self.client_id = "AXOE_xVY3cNceZ06BjwkpmYR9kxZkYCpDxdwyPyV1yqfrsESI5oso7X0l4KCvN300vUCoipAKhC_97bS"
-        self.client_secret = "EFkCv9V5wdhHAlaq0zrUSssL8I-4EHejuQa2w11oqLVlhCwIF2xqi5FFUDMznnJMfTkTahUiWE3-_PoG"
+        self.client_id = ""
+        self.client_secret = ""
         self.environment = SandboxEnvironment(client_id=self.client_id, client_secret=self.client_secret)
         self.client = PayPalHttpClient(self.environment)
